f619ca4ad3395145bc2599b8ad6c102c4d65a4d4
halil <svtr.hozcan@gmail.com>
2023-04-11 10:45:02 +0200

    add action method

--- CarStore/Program.cs
+++ CarStore/Program.cs
@@ -49,6 +49,25 @@ namespace CarStore
     {
         public static void Main(string[] args)
         {
+            System.Console.WriteLine("Welcome to the car store. First you must create some car inventory. Then you may add some cars to the shopping cart. Finally you may checkout which will give you a total value of the shopping cart.");
+
+            int action = chooseAction();
+
+            while (action != 0)
+            {
+                System.Console.WriteLine($"You chose {action}");
+                action = chooseAction();
+            }
+
+            static int chooseAction()
+            {
+                int choice = 0;
+                System.Console.WriteLine($"Choose an action (0) to quit (1) to add a new car to inventory (2) add car to cart (3) checkout");
+
+                choice = int.Parse(Console.ReadLine());
+                return choice;
+            }
+
             var mustang = new Car("Ford", "Mustang", 25000);
             var niro = new Car("Kia", "Niro", 12000);
 
@@ -60,5 +79,7 @@ namespace CarStore
             decimal total = store.Checkout();
             System.Console.WriteLine($"Store value is {total}");
         }
+
+
     }
 }
